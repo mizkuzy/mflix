3f827fa3e64bffacc6bf901a9bcd5ff9bc54fb23
mizkuzy <Allright22>
2019-12-02 10:06:20 +0300

    Ticket: Durable Writes. Update write concern when create a user

--- mflix/src/main/java/mflix/api/daos/UserDao.java
+++ mflix/src/main/java/mflix/api/daos/UserDao.java
@@ -2,6 +2,7 @@ package mflix.api.daos;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoWriteException;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.result.DeleteResult;
@@ -55,8 +56,8 @@ public class UserDao extends AbstractMFlixDao {
      * @return True if successful, throw IncorrectDaoOperation otherwise
      */
     public boolean addUser(User user) {
-        //TODO > Ticket: Durable Writes -  you might want to use a more durable write concern here!
-        usersCollection.insertOne(user);
+        //DONE > Ticket: Durable Writes -  you might want to use a more durable write concern here!
+        usersCollection.withWriteConcern(WriteConcern.MAJORITY).insertOne(user);
         return true;
         //TODO > Ticket: Handling Errors - make sure to only add new users
         // and not users that already exist.
