31394252c6b87125c274295f6d313168614665e3
mizkuzy <Allright22>
2019-12-03 10:31:32 +0300

    Ticket: Get Comments. Implement joining comments to movie

--- mflix/src/main/java/mflix/api/daos/MovieDao.java
+++ mflix/src/main/java/mflix/api/daos/MovieDao.java
@@ -64,9 +64,21 @@ public class MovieDao extends AbstractMFlixDao {
         List<Bson> pipeline = new ArrayList<>();
         // match stage to find movie
         Bson match = Aggregates.match(Filters.eq("_id", new ObjectId(movieId)));
+        // DONE> Ticket: Get Comments - implement the lookup stage that allows the comments to
+        //  retrieved with Movies.
+        Bson movieCommentsLookUp = new Document("$lookup",
+                new Document("from", "comments")
+                        .append("let",
+                                new Document("id", "$_id"))
+                        .append("pipeline", Arrays.asList(new Document("$match",
+                                        new Document("$expr",
+                                                new Document("$eq", Arrays.asList("$movie_id", "$$id")))),
+                                new Document("$sort",
+                                        new Document("date", -1L))))
+                        .append("as", "comments"));
         pipeline.add(match);
-        // TODO> Ticket: Get Comments - implement the lookup stage that allows the comments to
-        // retrieved with Movies.
+        pipeline.add(movieCommentsLookUp);
+
         Document movie = moviesCollection.aggregate(pipeline).first();
 
         return movie;
